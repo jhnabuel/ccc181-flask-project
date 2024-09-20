e93378c6255d6b962c1a946b23529e9af8aa03b6
jhnabuel <ninobano43@gmail.com>
2024-09-21 00:24:05 +0800

    Connect to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,45 @@
 # app/__init__.py
 from flask import Flask, render_template
+from flask import Flask
+from flask_mysqldb import MySQL
+from config import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+import mysql.connector
+
+
+mysql = MySQL()
+
+def test_db_connection(app):
+    try:
+        # Establish a connection to the database
+        connection = mysql.connector.connect(
+            host=app.config['MYSQL_HOST'],
+            user=app.config['MYSQL_USER'],
+            password=app.config['MYSQL_PASSWORD'],
+            database=app.config['MYSQL_DB']
+        )
+        
+        if connection.is_connected():
+            print("Database connection successful!")
+            return True
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return False
+    finally:
+        if connection.is_connected():
+            connection.close()
+
 def create_app():
     app = Flask(__name__)
 
+    app.config.from_mapping(
+        MYSQL_NAME=DB_NAME,
+        MYSQL_PASSWORD=DB_PASSWORD,
+        MYSQL_DB=DB_NAME,
+        MYSQL_HOST=DB_HOST,
+        MYSQL_USER=DB_USER
+    )
+
+    mysql.init_app(app)
 
     # Routing the master template (index.html) directly
     @app.route('/')
