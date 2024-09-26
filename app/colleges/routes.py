b740f5306219fb3b710eef6a7a0f65e9d2696fe8
jhnabuel <ninobano43@gmail.com>
2024-09-26 09:57:36 +0800

    Added search functionality (not working, to be debugged)

--- app/colleges/routes.py
+++ app/colleges/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template
+from flask import render_template, request
 from . import colleges  # Import the students Blueprint from __init__.py
 from .models import Colleges
 # Define a route for '/student' under the students blueprint
 @colleges.route('/college', methods=['GET'])
 def college_page():
-    colleges_list = Colleges.get_all_colleges()
-    return render_template('college/college.html', colleges=colleges_list)
+    search_term = request.args.get('search_term', '')  # Get the search term
+    search_by = request.args.get('search_by', 'college-name')  # Default to 'college-name'
+
+    colleges_list = Colleges.get_all_colleges(search_by, search_term)
+    return render_template('college/college.html', colleges=colleges_list, selected_info=search_by)
